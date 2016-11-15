db32aab150209dc1ab496c83c0b33d2632bde286
JP Sheehan <jp.sheehan2@gmail.com>
2016-11-14 20:01:41 -0500

    Moved public to app

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-var port = 1337;
 var express = require("express");
 var path = require('path');
+
+
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app')));
+app.set('port', process.env.PORT || 3000);
 
-/*
+
 app.get('/', function(req, res){
    //res.send('Hello World!');
    res.sendFile('./public/index.html');
 });
-*/
 
-app.listen(port);
-
-console.log('Server running at http://localhost:' + port);
+app.listen(app.get('port'),function(){
+   console.log('Server running at http://localhost:' + app.get('port'));
+});
