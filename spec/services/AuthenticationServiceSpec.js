92816049738020323b58af218f954ccce84d2e32
JP Sheehan <jp.sheehan2@gmail.com>
2016-11-24 21:24:05 -0500

    Completed Authorization tests

--- spec/services/AuthenticationServiceSpec.js
+++ spec/services/AuthenticationServiceSpec.js
@@ -35,11 +35,12 @@ describe('Authentication Service Test',function(){
 
    describe('Authentication Test', function() {
 
-      var AuthenticationServce, $rootScope, $cookieStore;
-      beforeEach(inject(function(_AuthenticationService_, _$rootScope_, _$cookieStore_){
+      var AuthenticationServce, $rootScope, $cookieStore, $http;
+      beforeEach(inject(function(_AuthenticationService_, _$rootScope_, _$cookieStore_, _$http_){
          AuthenticationService = _AuthenticationService_;
          $rootScope = _$rootScope_;
          $cookieStore = _$cookieStore_;
+         $http = _$http_;
       }));
 
       describe('Login', function() {
@@ -67,32 +68,54 @@ describe('Authentication Service Test',function(){
          var username = 'user1234';
          var password = 'notAgr8PW';
 
+         beforeEach(function(){
+            AuthenticationService.SetCredentials(username,password);
+         });
+
          it('Stores the username in rootScope',function() {
-         AuthenticationService.SetCredentials(username,password);
             expect($rootScope.globals.currentUser.username).toBe(username);
          });
 
-         it('Stores encoded authorization', function() {
-            AuthenticationService.SetCredentials(username,password);
+         it('Stores encoded authorization in rootScope', function() {
             expect($rootScope.globals.currentUser.authdata.indexOf(username)).toBe(-1);
             expect($rootScope.globals.currentUser.authdata.indexOf(password)).toBe(-1);
             expect($rootScope.globals.currentUser.authdata).toBeTruthy();
          });
 
+         it('Stores encoded authorization in header',function() {
+            expect($http.defaults.headers.common.Authorization.substr(0,5)).toBe('Basic');
+            expect($http.defaults.headers.common.Authorization.substr(5)).toBeTruthy();
+         });
+
          it('Stores globals in cookie', function() {
-            AuthenticationService.SetCredentials(username,password);
             expect($rootScope.globals).toEqual($cookieStore.get('globals'));
          });
 
       });
 
-     /*
       describe('ClearCredentials',function() {
          var username = 'user1234';
          var password = 'notAgr8PW';
          var authdata;
-         */
 
+         beforeEach(function(){
+            AuthenticationService.SetCredentials(username,password);
+            AuthenticationService.ClearCredentials(username, password);
+         });
+
+         it('Clears $rootScope globals',function(){
+            expect($rootScope.globals).toEqual({});
+         });
+
+         it('Clears cookie globals',function(){
+            expect($cookieStore.get('globals')).not.toBeDefined();
+         });
+
+         it('Clears http header', function() {
+            expect($http.defaults.headers.common.Authorization).toBe('Basic');
+         });
+            
+      });
 
    });
 });
