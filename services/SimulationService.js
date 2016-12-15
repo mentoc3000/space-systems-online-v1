d1caa2d4e16c4df8adf775dfb9c28a19b13f1cb6
JP Sheehan <jp.sheehan2@gmail.com>
2016-12-15 15:55:13 -0500

    Simulator displays GMAT output

--- services/SimulationService.js
+++ services/SimulationService.js
@@ -1,5 +1,6 @@
-var Q = require('q');
-var Promise = Q.Promise;
+// var Q = require('q');
+// var Promise = Q.Promise;
+var gmat = require('../lib/gmat');
 
 var service = {};
 
@@ -8,10 +9,14 @@ service.reverse = reverse;
 module.exports = service;
 
 function reverse(input) {
+   /*
    var splitString = input.split('');
    var reverseArray = splitString.reverse();
    var joinArray = reverseArray.join('');
    return new Promise(function resolver(resolve, reject) {
       resolve(joinArray);
    });
+   */
+
+   return gmat.runScript();
 }
